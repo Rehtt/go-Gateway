bcd564792aa318b8c1f003b4556eb4380bc5e43d
yehaoyang <dsreshiram@gmail.com>
2021-11-06 18:34:20 +0800

    update

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"go-Gateway/config"
+	"github.com/spf13/viper"
+	initConfig "go-Gateway/config"
 	"go-Gateway/router"
 	"go-Gateway/router/middleware"
 	"os"
@@ -14,9 +16,14 @@ var (
 	configFile = flag.String("c", "./config/config.yaml", "配置文件地址")
 )
 
+type (
+	port string
+	host string
+)
+
 func init() {
 	flag.Parse()
-	if err := config.InitConfig(*configFile); err != nil {
+	if err := initConfig.InitConfig(*configFile); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
@@ -39,16 +46,48 @@ func main() {
 		g,
 		// 中间件
 		middleware.Block,
-		middleware.TLS,
 		//middleware.Options,
 		//middleware.NoCache,
 	)
+	//var a, b, c tls.Certificate
+	//conf := &tls.Config{Certificates: []tls.Certificate{a, b, c}}
+	//t, _ := tls.Listen("tcp", "0.0.0.0:443", conf)
+	//g.RunListener(t)
 	go g.Run(":8080")
 	//todo 读取配置文件
-	//configs:=viper.Get("app")
-	//for _,config:=range configs.([]interface{}){
-	//}
+	configs := viper.Get("app")
+
+	tlss := map[port]map[host]tls.Certificate{}
+	for _, c := range configs.([]interface{}) {
+		config := c.(map[string]interface{})
+		var addr []string
+		var port []string
+		listenAddress(config, &addr, &port, &tlss)
+
+	}
 
 	//http.ListenAndServe(viper.GetString("server.addr")+":"+viper.GetString("server.port"), g)
 	select {}
+}
+func listenAddress(config map[string]interface{}, a, p *[]string, tlss interface{}) error {
+	listen := config["listen"].(map[string]interface{})
+	if listen != nil {
+		if listen["addr"] != nil {
+			*a = listen["addr"].([]string)
+		}
+		if listen["port"] != nil {
+			*p = listen["port"].([]string)
+		}
+	}
+	t := config["tls"].(map[string]interface{})
+	if t != nil && t["cert"] != nil && t["key"] != nil {
+		f, err := tls.LoadX509KeyPair(t["cert"].(string), t["key"].(string))
+		if err != nil {
+			return err
+		}
+		for _, po := range *p {
+			// todo
+			//tlss.(map[port]map[host]tls.Certificate)[po]=f
+		}
+	}
 }
